dd4a908699ff6b80b4a9fadb95e2faee899deaac
Mark Jeff van Haastert <mjvanhaastert@hotmail.com>
2017-06-16 12:44:54 +0200

    Update raspwebvla.sh

--- raspwebvla.sh
+++ raspwebvla.sh
@@ -7,40 +7,72 @@
 #
 #sudo ./raspwebvla.sh
 
+clear
+
+echo "###################################################################################"
 echo "###################################################################################"
 echo "Please be Patient: Installation will start now.......and it will take some time :)"
+echo "You will be asked to setup a password for mysql and answer some questions"
+echo "So keep a pen and paper by hand to write stuff down"
+echo "###################################################################################"
 echo "###################################################################################"
 
 #Update the repositories
 
-sudo apt-get update
+sudo apt-get -y --force-yes update
+sudo apt-get -y --force-yes upgrade
+sudo apt-get -y --force-yes dist-upgrade
 
+clear
+
+echo "###################################################################################"
 echo "###################################################################################"
 echo "Installing apache2 , mysql server, phpmyadmin and client and all the plugins needed"
 echo "###################################################################################"
+echo "###################################################################################"
 
-apt-get install apache2
-apt-get install php7.0-mysql php7.0-curl php7.0-json php7.0-cgi  php7.0 libapache2-mod-php7.0 mysql-client
-
-#The following commands set the MySQL root password to WebVla-01! when you install the mysql-server package.
-
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password WebVla-01!'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password WebVla-01!'
-
-apt-get install mysql-server
-mysql_secure_installation
+sudo apt-get install apache2
+sudo apt-get install mysql-server mysql-client
+sudo apt-get install php7.0-mysql php7.0-curl php7.0-json php7.0-cgi  php7.0 libapache2-mod-php7.0
 apt-get install phpmyadmin
 
+mysql_secure_installation
+
+clear
+
 echo "###################################################################################"
-echo "Creating a folder called drupal in the /home/ubuntu"
-echo "Then downloading the drupal core files and modules"
+echo "###################################################################################"
+echo "phpmyadmin is install and can be accessed by typing in the ip of the webserver"
+echo "The username of phpmyadmin is root and the password you created"
+echo "###################################################################################"
 echo "###################################################################################"
 
+sleep -s 10
+
+clear
+
+echo "###################################################################################"
+echo "###################################################################################"
+echo "Creating folders in the /home/ubuntu"
+echo "Then downloading the drupal core files, modules and civicrm"
+echo "###################################################################################"
+echo "###################################################################################"
+
+sudo mkdir /home/civicrm
+cd /home/civicrm
+sudo wget https://download.civicrm.org/civicrm-4.7.20-drupal.tar.gz
+sudo tar -xvzf civicrm-4.7.20-drupal.tar.gz
+wget https://download.civicrm.org/civicrm-4.7.20-l10n.tar.gz
+sudo tar xvzf civicrm-4.7.20-l10n.tar.gz
+rm -r civicrm-4.7.20-drupal.tar.gz civicrm-4.7.20-l10n.tar.gz
 sudo mkdir /home/ubuntu/drupal
 sudo mkdir /home/ubuntu/modules
 cd /home/ubuntu/drupal/
 sudo wget https://ftp.drupal.org/files/projects/drupal-7.55.tar.gz
-tar -xvzf drupal-7.55.tar.gz
+sudo tar -xvzf drupal-7.55.tar.gz
+sudo rm -r drupal-7.55.tar.gz
+cd drupal-7.55/profiles/standard/translations
+sudo wget ftp.drupal.org/files/translations/7.x/drupal/drupal-7.55.nl.po
 cd /home/ubuntu/modules
 sudo wget https://ftp.drupal.org/files/projects/ckeditor-7.x-1.17.tar.gz
 sudo wget https://ftp.drupal.org/files/projects/views-7.x-3.16.tar.gz
@@ -53,10 +85,30 @@ sudo wget https://ftp.drupal.org/files/projects/webform_layout-7.x-2.3.tar.gz
 tar -xvzf *
 
 echo "###################################################################################"
+echo "###################################################################################"
 echo "Moving everything in the right place"
 echo "###################################################################################"
+echo "###################################################################################"
 
-mv -r ckeditor views ctools rules webform webform_civicrm options_element webform_layout -t /home/ubuntu/drupal/modules
+cd /home/ubuntu/civicrm/
+sudo mv -r civicrm /home/ubuntu/drupal/modules
+sudo mv -r ckeditor views ctools rules webform webform_civicrm options_element webform_layout -t /home/ubuntu/drupal/modules
+cd /home/ubuntu/drupal/drupal-7.55
+sudo mv -r * /var/www/html
+
+clear
+
+echo "###################################################################################"
+echo "###################################################################################"
+echo "Drupal has bin moved to /var/www/html and can be accessed by typing in the ip of the webserver in your browers
+echo "There you can start installing drupal and setting up a admin account"
+echo "###################################################################################"
+echo "###################################################################################"
+
+sleep -s 10
+
+clear
+
 
 
 #Restart all the installed services to verify that everything is installed properly
